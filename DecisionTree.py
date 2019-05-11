1cd874ba1bb96c042ce50259268858d8dae69aed
Tobias Schäfer <tobias.schaefer@media-t.de>
2019-05-11 13:24:32 +0200

    Add decision trees (pure python)

---
+++ DecisionTree.py
@@ -0,0 +1,35 @@
+from sklearn.metrics import f1_score
+import pandas as pd
+import functions
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+
+X, y = functions.get_data()
+
+# split dataset into train and test data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=10)
+
+clf = DecisionTreeClassifier(criterion="entropy", max_depth=10, splitter="random", min_samples_split=10,
+                             max_features=None)
+clf.fit(X_train, y_train.values.ravel())
+
+y_pred = clf.predict(X_test)
+
+f1 = f1_score(y_test, y_pred, average='weighted')
+
+print("F1 score is = {0}".format(f1))
+
+f = open("results/files/clf_results.txt", "w+")
+f.write("Final F1 score = {0} \n".format(f1))
+f.close()
+
+result = pd.DataFrame(y_pred, columns=["y_pred"])
+
+result['y_test'] = y_test["Loan Status"].values
+
+result.reset_index(drop=True, inplace=True)
+X_test.reset_index(drop=True, inplace=True)
+
+result = pd.concat([result, X_test], axis=1)
+
+result.to_csv("results/datasets/clf_y_pred.csv")
---
+++ DecisionTree_paramTuning.py
@@ -0,0 +1,34 @@
+import functions
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import GridSearchCV
+
+x_train, y_train = functions.get_data()
+
+criterion = ['gini', 'entropy']
+max_depth = list(range(1, 21, 1)) + list(range(20, 210, 10))
+splitter = ['best', 'random']
+min_samples_split = list(range(2, 50, 2))
+max_features = ['auto', 'sqrt', 'log2', None]
+
+params = dict(criterion=criterion, max_depth=max_depth, splitter=splitter, min_samples_split=min_samples_split,
+              max_features=max_features)
+
+clf = DecisionTreeClassifier()
+
+clfCV = GridSearchCV(clf, params, cv=10, scoring='f1', verbose=10, n_jobs=-1)
+
+clfCV.fit(x_train, y_train.values.ravel())
+
+best_score = clfCV.best_score_
+
+best_estimator = clfCV.best_estimator_
+
+best_params = clfCV.best_params_
+
+f = open("results/files/clf_tuning_results.txt", "w+")
+
+f.write("Best score = {0} \n".format(best_score))
+f.write("Best estimator = {0} \n".format(best_estimator))
+f.write("Best params = {0} \n".format(best_params))
+
+f.close()
